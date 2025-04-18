4753fb4615c18acf383c917b984814642656bfd6
Aerendir <hello@aerendir.me>
2025-04-18 13:24:24 +0200

    Apply fixes.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,18 +3,14 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Coommercio.
+ * This file is part of the Serendipity HQ Monolog HTML Line Formatter.
  *
- * Copyright (c) Adamo Aerendir Crespi <hello@aerendir.me>.
+ * Copyright (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
  *
- * This code is to consider private and non disclosable to anyone for whatever reason.
- * Every right on this code is reserved.
- *
- * For the full copyright and license information, please view the LICENSE file that
- * was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-use DG\BypassFinals;
 /*
  * This file is part of Coommercio.
  *
@@ -26,8 +22,6 @@ use DG\BypassFinals;
  * For the full copyright and license information, please view the LICENSE file that
  * was distributed with this source code.
  */
-
-use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
